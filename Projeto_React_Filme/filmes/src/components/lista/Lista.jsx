2f3d66b3f689b5c63d38718c1bf39f8b9a870c04
SOPHIA ALMEIDA OLIVEIRA <44098786893@info.local>
2025-05-15 16:53:20 -0300

    feat: deletar feito em genero

--- Projeto_React_Filme/filmes/src/components/lista/Lista.jsx
+++ Projeto_React_Filme/filmes/src/components/lista/Lista.jsx
@@ -19,21 +19,34 @@ const Lista = (props) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="item_lista">
-                            <td data-cell ="Nome">Velozes e Furiosos</td>
+                        {/* VERIFICAR SE A LISTA ESTA VINDO VAZIA */}
+                        {props.lista && props.lista.length > 0 ?(
+
+                            // vamos mapear os itens da lista 
+                            props.lista.map((item) => (
+                            <tr className="item_lista" key={item.idGenero}>
+                            <td data-cell ="Nome">
+                                {item.nome}
+                            </td>
                             <td data-cell ="Genero" style={{display:props.visibilidade}}>Ação</td>
                             <td data-cell ="Editar"><img src={Editar} alt="Imagem de uma caneta" /></td>
-                            <td data-cell ="Excluir"><img src={Excluir} alt="Lixeira" /></td>
+                            <td data-cell ="Excluir">
+                            <button>
+
+                                <img src={Excluir} alt="Lixeira" onClick = {()=> (props.funcExcluirGenero(item))} />
+                            </button>
+                            </td>
                         </tr>
+                            ))
+                      
+                        )
+                    :
+                    (
+                    <p> Nenhum genero foi encontrado </p>
+                    )
+                    }
                     </tbody>
-                    <tbody>
-                        <tr className="item_lista">
-                            <td data-cell ="Nome">Velozes e Furiosos</td>
-                            <td data-cell ="Genero" style={{display:props.visibilidade}}>Ação</td>
-                            <td data-cell ="Editar"><img src={Editar} alt="Imagem de uma caneta" /></td>
-                            <td data-cell ="Excluir"><img src={Excluir} alt="Lixeira" /></td>
-                        </tr>
-                    </tbody>
+                    
                 </table>
             </div>
         </section>
--- Projeto_React_Filme/filmes/src/pages/cadastroGenero/CadastroGenero.jsx
+++ Projeto_React_Filme/filmes/src/pages/cadastroGenero/CadastroGenero.jsx
@@ -1,28 +1,172 @@
-import Header from "../../components/header/Header"
-import Footer from "../../components/footer/Footer"
-import Cadastro from "../../components/cadastro/Cadastro"
-import Lista from "../../components/lista/Lista"
+import { useEffect, useState } from "react";
+import api from "../../Services/services"
+import Swal from 'sweetalert2'
+
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import Cadastro from "../../components/cadastro/Cadastro";
+import Lista from "../../components/lista/Lista";
+
 
 const CadastroGenero = () => {
+
+
+    const [genero, setGenero] = useState("");
+    const [listaGenero, setListaGenero] = useState([]);
+    // const [deletarGenero, setDeletarGenero] = useState ();
+
+    // DELETAR INICIO
+    function deletar(titulo, mensagem) {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true
+        }).then((result) => {
+            if (result.isConfirmed) {
+                swalWithBootstrapButtons.fire({
+                    // aqui tem bagulho
+                    title: titulo,
+                    text: mensagem,
+                    icon: "warning"
+                });
+            } else if (
+                /* Read more about handling dismissals below */
+                result.dismiss === Swal.DismissReason.cancel
+            ) {
+                swalWithBootstrapButtons.fire({
+                    // aqui tem bagulho tambem
+                    title: titulo,
+                    text: mensagem,
+                    icon: "success"
+                });
+            }
+        });
+
+    }
+
+
+    function alerta(icone, mensagem) {
+
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+        });
+        Toast.fire({
+            icon: icone,
+            title: mensagem
+        });
+    }
+
+
+    async function cadastrarGenero(evt) {
+        evt.preventDefault();
+
+        //verificar se o input esta vindo vazio
+        if (genero.trim != "") {
+            //try => tentar(o esperado)
+            //catch => lanca a excecao
+            try {
+                //cadastrar um genero: post
+                await api.post("/genero", { nome: genero });
+                alerta("success", "Cadastro feito com sucesso!")
+                setGenero()
+            } catch (error) {
+                alerta("error", "Erro! Entre em contato com o suporte do servisso")
+                console.log(error);
+            }
+        } else {
+            alerta("error", "Erro! Preencha o campo")
+        }
+    }
+
+    async function listarGenero() {
+        try {
+            const resposta = await api.get("genero");
+            // console.log(resposta.data)
+            setListaGenero(resposta.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // funcao de excluir o genero;)//
+    async function deletarGenero(Generoid) {
+        try {
+            await api.delete(`genero/${Generoid.idGenero}`)
+        } catch (error) { 
+            deletar("Tem certeza que deseja deletar","Essa ação não podera ser desfeita","Voce canselou sua ação","O genero não foi deletado")
+            console.log(error)
+        }
+
+
+
+    }
+
+
+    //teste: validar genero
+
+    //useEffect <funcao> <dependencia>
+    // useEffect(() => {
+    //     console.log(genero);
+    // }, [genero]);
+
+    // fim do teste
+
+    useEffect(() => {
+        listarGenero();
+    }, [])
+
+
+
     return (
         <>
             <Header />
             <main>
-                <Cadastro
-                    tituloCadastro="Cadastro de Gênero"
+                <Cadastro tituloCadastro="cadastro de Genero"
                     visibilidade="none"
-                    placeholder="gênero"
-                    />
-                <Lista 
-                listaDeCadastro="Lista de Gêneros" 
-                visibilidade="none"
-                
+                    placeholder="Genero"
+                    //Atribuindo a funcao:
+                    funcCadastro={cadastrarGenero}
+                    //Atribuindo o valor ao input:
+                    valorInput={genero}
+                    //Atribuindo a funcao que atualiza meu genero:
+                    setValorInput={setGenero}
                 />
+                <Lista
+                    tituloLista="Lista de Genero"
+                    visibilidade="none"
+
+                    // atribuir para lista, o ney estado atual:
+
+                    lista={listaGenero}
+
+                    funcExcluirGenero={deletarGenero}
+
+                />
+
             </main>
             <Footer />
         </>
-
     )
 }
+
 
 export default CadastroGenero;
